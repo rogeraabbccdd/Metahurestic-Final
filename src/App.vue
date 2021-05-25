e2e8581506049f3886da9919d832e3a1c38301aa
rogeraabbccdd <rogeraabbccdd@gmail.com>
2021-05-25 18:29:02 +0800

    新增搜尋法選項

--- src/App.vue
+++ src/App.vue
@@ -30,13 +30,18 @@
         .row.my-3
           label.col-2.col-form-label(for="inputS") 演算法
           .col-10
-            select.form-select#inputS(v-model="heuristic")
+            select.form-select#inputS(v-model="heuristic" :disabled="heuristicDisabled")
               option(v-for="(h, i) in heuristicOptions" :key="i" :value="h.value") {{ h.name }}
+        .row.my-3
+          label.col-2.col-form-label(for="inputF") 搜尋法
+          .col-10
+            select.form-select#inputF(v-model="finder")
+              option(v-for="(f, i) in finderOptions" :key="i" :value="f.value") {{ f.name }}
         input.btn.btn-primary(type="button" value="產生路徑" @click="getPath")
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue'
+import { ref, reactive, computed } from 'vue'
 import PF from 'pathfinding'
 
 // 0 = walkable
@@ -51,12 +56,25 @@ const matrix = reactive([
 ])
 
 const heuristic = ref('manhattan')
+const finder = ref('AStarFinder')
 
 const heuristicOptions = reactive([
   { value: 'manhattan', name: '曼哈頓距離' },
   { value: 'chebyshev', name: '柴比雪夫距離' },
   { value: 'euclidean', name: '歐幾里得距離' },
   { value: 'octile', name: 'Octile 距離' }
+])
+
+const finderOptions = reactive([
+  { value: 'AStarFinder', name: 'A*' },
+  { value: 'BestFirstFinder', name: '最良優先搜尋' },
+  { value: 'BreadthFirstFinder', name: '廣度優先搜尋' },
+  { value: 'DijkstraFinder', name: 'dijkstra' },
+  { value: 'JumpPointFinder', name: '跳點搜索' },
+  { value: 'BiAStarFinder', name: '雙向 A*' },
+  { value: 'BiBestFirstFinder', name: '雙向最良優先搜尋' },
+  { value: 'BiBreadthFirstFinder', name: '雙向廣度優先搜尋' },
+  { value: 'BiDijkstraFinder', name: '雙向 dijkstra' },
 ])
 
 const resultPath = reactive({show: false, length: 0})
@@ -109,12 +127,12 @@ const getPath = () => {
       }
     }
   }
-  const finder = new PF.AStarFinder({
+  const pfinder = new PF[finder.value]({
     heuristic: PF.Heuristic[heuristic.value]
   })
   const grid = new PF.Grid(matrixClone)
   // find path and remove start/end
-  const path = finder.findPath(start.x, start.y, end.x, end.y, grid).slice(1, -1)
+  const path = pfinder.findPath(start.x, start.y, end.x, end.y, grid).slice(1, -1)
   for (const i in path) {
     matrix[path[i][1]][path[i][0]] = 4
   }
@@ -122,4 +140,12 @@ const getPath = () => {
   resultPath.length = path.length
 }
 
+const heuristicDisabled = computed(() => {
+  if(finder.value === 'AStarFinder' || finder.value === 'BestFirstFinder' ||
+    finder.value === 'BiAStarFinder' || finder.value === 'BiBestFirstFinder'
+  ) {
+    return false
+  }
+  return true
+})
 </script>
